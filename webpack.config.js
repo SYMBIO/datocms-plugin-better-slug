2cffeae02f23ef773dde503e5779072a8dec1657
Martin Cajthaml <martin.cajthaml@symbio.agency>
2019-11-25 12:47:08 +0100

    tags

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,5 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const HtmlWebpackIncludeAssetsPlugin = require('html-webpack-include-assets-plugin');
+const HtmlWebpackTagsPlugin = require('html-webpack-tags-plugin');
 
 const isProduction = process.env.NODE_ENV === 'production';
 
@@ -43,13 +43,13 @@ module.exports = {
       title: 'DatoCMS Plugin',
       minify: isProduction,
     }),
-    new HtmlWebpackIncludeAssetsPlugin({
+    new HtmlWebpackTagsPlugin({
       append: false,
       publicPath: '',
-      assets: [
-        'https://unpkg.com/datocms-plugins-sdk/dist/sdk.js',
-        'https://unpkg.com/datocms-plugins-sdk/dist/sdk.css',
-      ]
+      tags: [
+        { type: 'css', path: 'https://unpkg.com/datocms-plugins-sdk/dist/sdk.css' },
+        { type: 'js', path: 'https://unpkg.com/datocms-plugins-sdk/dist/sdk.js' },
+      ],
     }),
   ].filter(Boolean),
 }
